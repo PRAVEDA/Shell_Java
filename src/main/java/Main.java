7eced0cbafa27ae9b92cec98748870e85be11b00
Praveda <pravedanalla@gmail.com>
2026-06-13 08:17:31 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,17 @@ public class Main {
         while (true) {
             System.out.print("$ ");
 
-            String input = scanner.nextLine();
+            String input = scanner.nextLine() ; 
 
-            if(input.equals("exit")){
-                break ; 
-            }
-
-            System.out.println(input + ": command not found");
+           if(input.equals("exit")){
+            break ; 
+           }
+           else if (input.startsWith("echo")){
+            System.out.println(input.substring(5)) ; 
+           }
+           else{
+            System.out.println(input +": command not fount " )
+           }
         }
     }
 }
