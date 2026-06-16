f9eb21c6f3d91fc66b3a795bcb7788bb5fa8083a
Praveda <pravedanalla@gmail.com>
2026-06-16 11:15:42 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,7 +30,7 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
 
-            if (!inSingleQuotes && !inDoubleQuotes && ch == '\\') {
+            if (ch == '\\' && !inSingleQuotes && !inDoubleQuotes) {
                 if (i + 1 < input.length()) {
                     current.append(input.charAt(i + 1));
                     i++;
