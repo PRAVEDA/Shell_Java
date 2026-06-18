c90ad103c7cee619b9e776769ab75ae323f4cf7b
Praveda <pravedanalla@gmail.com>
2026-06-18 15:42:36 +0530

    codecrafters submit [skip ci]

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-#
-# This script is used to run your program on CodeCrafters
-#
-# This runs after .codecrafters/compile.sh
-#
-# Learn more: https://codecrafters.io/program-interface
+set -e
 
-set -e # Exit on failure
-
+stty -icanon min 1 time 0 2>/dev/null || true
 exec java --enable-native-access=ALL-UNNAMED --enable-preview -jar /tmp/codecrafters-build-shell-java/codecrafters-shell.jar "$@"
